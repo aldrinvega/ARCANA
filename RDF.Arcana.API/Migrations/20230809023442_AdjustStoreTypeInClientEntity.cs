c0657b3c88a54f40effb4305a68232d5cda3bb54
aldrin0408av <aldrinmundovega@gmail.com>
2023-08-09 11:01:27 +0800

    Migrations

---
+++ RDF.Arcana.API/Migrations/20230809023442_AdjustStoreTypeInClientEntity.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RDF.Arcana.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdjustStoreTypeInClientEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "store_type",
+                table: "clients");
+
+            migrationBuilder.AddColumn<int>(
+                name: "store_type_id",
+                table: "clients",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_clients_store_type_id",
+                table: "clients",
+                column: "store_type_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_clients_store_types_store_type_id",
+                table: "clients",
+                column: "store_type_id",
+                principalTable: "store_types",
+                principalColumn: "id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_clients_store_types_store_type_id",
+                table: "clients");
+
+            migrationBuilder.DropIndex(
+                name: "ix_clients_store_type_id",
+                table: "clients");
+
+            migrationBuilder.DropColumn(
+                name: "store_type_id",
+                table: "clients");
+
+            migrationBuilder.AddColumn<string>(
+                name: "store_type",
+                table: "clients",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
