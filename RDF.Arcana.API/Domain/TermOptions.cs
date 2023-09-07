f125a09159012e13b6e55c923119e60b447c6322
aldrin0408av <aldrinmundovega@gmail.com>
2023-09-07 10:42:31 +0800

    Add TermOptions

---
+++ RDF.Arcana.API/Domain/TermOptions.cs
@@ -0,0 +1,20 @@
+﻿using RDF.Arcana.API.Common;
+
+namespace RDF.Arcana.API.Domain
+{
+    public class TermOptions : BaseEntity
+    {
+        public int ClientId { get; set; }
+        public int TermId { get; set; }
+        public int CreditLimit { get; set; }
+        public int TermDaysId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public int AddedBy { get; set; }
+
+        public Clients Clients { get; set; }
+        public User AddedByUser { get; set; }
+        public TermDays TermDays { get; set; }
+        public Terms Terms { get; set; }
+    }
+}
---
+++ RDF.Arcana.API/Migrations/20230901015305_AddClientIdonTermsOptionsEntity.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RDF.Arcana.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddClientIdonTermsOptionsEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "client_id",
+                table: "term_options",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_term_options_client_id",
+                table: "term_options",
+                column: "client_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_term_options_clients_clients_id",
+                table: "term_options",
+                column: "client_id",
+                principalTable: "clients",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_term_options_clients_clients_id",
+                table: "term_options");
+
+            migrationBuilder.DropIndex(
+                name: "ix_term_options_client_id",
+                table: "term_options");
+
+            migrationBuilder.DropColumn(
+                name: "client_id",
+                table: "term_options");
+        }
+    }
+}
