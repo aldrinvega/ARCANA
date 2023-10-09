e9f012fb68a4481f3911bc4872cbd7f1e8a6e2ce
aldrinvega <aldrinmundovega@gmail.com>
2023-10-09 16:05:48 +0800

    Add Migrations
    Signed-off-by: aldrinvega <aldrinmundovega@gmail.com>

---
+++ RDF.Arcana.API/Migrations/20231009080231_AdjustNullableTermOptionCreditOptions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RDF.Arcana.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdjustNullableTermOptionCreditOptions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "credit_limit",
+                table: "term_options",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "credit_limit",
+                table: "term_options",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
