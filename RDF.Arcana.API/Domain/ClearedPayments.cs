e144f70c85e4335ea1058c9f0b8973083ab7f2e6
it.ralphedwardgesguerra@gmail.com <it.ralphedwardgesguerra@gmail.com>
2024-04-24 09:04:05 +0800

    042424Ticket

---
+++ RDF.Arcana.API/Domain/ClearedPayments.cs
@@ -0,0 +1,16 @@
+﻿using RDF.Arcana.API.Common;
+
+namespace RDF.Arcana.API.Domain
+{
+    public class ClearedPayments : BaseEntity
+    {
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+        public int AddedBy { get; set; }
+        public int? ModifiedBy { get; set; }
+        public bool IsActive { get; set; }
+        public virtual User AddedByUser { get; set; }
+        public virtual User ModifiedByUser { get; set; }
+        public virtual ICollection<PaymentTransaction> PaymentTransactions { get; set; }
+    }
+}
