c9943a1cc9777b8a8e8110c3edbc7eb0c79b7125
it.ralphedwardgesguerra@gmail.com <it.ralphedwardgesguerra@gmail.com>
2024-05-20 16:43:33 +0800

    GetAllLF&AP

---
+++ RDF.Arcana.API/Features/Listing Fee/GetAllListingFeeBalance.cs
@@ -0,0 +1,68 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using RDF.Arcana.API.Common;
+using RDF.Arcana.API.Data;
+
+namespace RDF.Arcana.API.Features.Listing_Fee
+{
+    [Route("api/ListingFee"), ApiController]
+    public class GetAllListingFeeBalance : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public GetAllListingFeeBalance(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("GetAllListingFeeTotals")]
+        public async Task<IActionResult> GetAll()
+        {
+            var query = new GetAllListingFeeTotalsQuery();
+            try
+            {
+                var result = await _mediator.Send(query);
+                return Ok(result);
+            }
+            catch (System.Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        public class GetAllListingFeeTotalsQuery : IRequest<Result> { }
+        public class GetAllListingFeesTotalsResult
+        {            
+            public string BusinessName{ get; set; }
+            public string FullName { get; set; }
+            public decimal RemainingBalance { get; set; }
+        }
+
+        public class Handler : IRequestHandler<GetAllListingFeeTotalsQuery, Result>
+        {
+            private readonly ArcanaDbContext _context;
+            public Handler(ArcanaDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Result> Handle(GetAllListingFeeTotalsQuery request, CancellationToken cancellationToken)
+            {
+                var listingFees = await _context.ListingFees
+                    .Include(c => c.Client)
+                    .Where(lf => lf.Status == Status.Approved &&
+                                 lf.IsActive)
+                    .GroupBy(lf => new { lf.Client.Fullname, lf.Client.BusinessName })
+                    .Select(lf => new GetAllListingFeesTotalsResult 
+                            {                                
+                                BusinessName = lf.Key.BusinessName,
+                                FullName = lf.Key.Fullname,
+                                RemainingBalance = lf.Sum(lf => lf.Total)
+                            })
+                    .OrderBy(lf => lf.BusinessName)
+                    .ToListAsync(cancellationToken);
+
+                return Result.Success(listingFees);
+            }
+        }
+    }
+}
---
+++ RDF.Arcana.API/Features/Sales Management/Advance Payment/GetAllAdvancePaymentBalance.cs
@@ -0,0 +1,69 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using RDF.Arcana.API.Common;
+using RDF.Arcana.API.Data;
+
+namespace RDF.Arcana.API.Features.Sales_Management.Advance_Payment
+{
+    [Route("api/AdvancePayment"), ApiController]
+    public class GetAllAdvancePaymentBalance : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public GetAllAdvancePaymentBalance(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("GetAllAdvancePaymentTotals")]
+        public async Task<IActionResult> GetAll()
+        {
+            var query = new GetAllAdvancePaymentTotalsQuery();
+            try
+            {
+                var result = await _mediator.Send(query);
+                return Ok(result);
+            }
+            catch (Exception ex) 
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        public class GetAllAdvancePaymentTotalsQuery : IRequest<Result> { }
+
+        public class GetAllAdvancePaymentTotalsResult
+        {
+            public string BusinessName { get; set; }
+            public string FullName { get; set; }
+            public decimal RemainingBalance { get; set; }
+        }
+
+        public class Handler : IRequestHandler<GetAllAdvancePaymentTotalsQuery, Result>
+        {
+            private readonly ArcanaDbContext _context;
+            public Handler(ArcanaDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Result> Handle(GetAllAdvancePaymentTotalsQuery request, CancellationToken cancellationToken)
+            {
+                var advancePayments = await _context.AdvancePayments
+                    .Include(c => c.Client)
+                    .Where(ap => ap.Status != Status.Voided &&
+                                 ap.IsActive)
+                    .GroupBy(ap => new { ap.Client.Fullname, ap.Client.BusinessName })
+                    .Select(ap => new GetAllAdvancePaymentTotalsResult
+                    {
+                        BusinessName = ap.Key.BusinessName,
+                        FullName = ap.Key.Fullname,
+                        RemainingBalance = ap.Sum(ap => ap.RemainingBalance)
+                    })
+                    .OrderBy(ap => ap.BusinessName)
+                    .ToListAsync(cancellationToken);
+
+                return Result.Success(advancePayments);
+            }
+        }
+    }
+}
