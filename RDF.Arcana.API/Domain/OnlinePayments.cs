44a5f582473197968bd9a0ff9a1a0d2e9047cadb
it.ralphedwardgesguerra@gmail.com <it.ralphedwardgesguerra@gmail.com>
2024-06-24 16:46:15 +0800

    asdasd

--- RDF.Arcana.API/Domain/OnlinePayments.cs
+++ RDF.Arcana.API/Domain/OnlinePayments.cs
@@ -6,7 +6,7 @@ namespace RDF.Arcana.API.Domain
     {
         public string OnlinePlatform { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.Now;
-        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+        public DateTime? UpdatedAt { get; set; }
         public int AddedBy { get; set; }
         public int? ModifiedBy { get; set; }
         public bool IsActive { get; set; } = true;
