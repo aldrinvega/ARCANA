6894a78b5b2e63ab7ec02f1949c200d262c023f3
it.ralphedwardgesguerra@gmail.com <it.ralphedwardgesguerra@gmail.com>
2024-07-09 16:55:36 +0800

    Ongoing GetListingFeeByClientId

--- RDF.Arcana.API/Features/Listing Fee/GetListingFeeBalanceByClientId.cs
+++ RDF.Arcana.API/Features/Listing Fee/GetListingFeeBalanceByClientId.cs
@@ -1,49 +1,85 @@
-﻿//using RDF.Arcana.API.Common;
-//using RDF.Arcana.API.Data;
-//using RDF.Arcana.API.Features.Listing_Fee.Errors;
+﻿using Microsoft.AspNetCore.Mvc;
+using RDF.Arcana.API.Common;
+using RDF.Arcana.API.Data;
+using RDF.Arcana.API.Features.Listing_Fee.Errors;
+using static RDF.Arcana.API.Features.Client.All.GetClientOtherExpensesById;
 
-//namespace RDF.Arcana.API.Features.Listing_Fee;
+namespace RDF.Arcana.API.Features.Listing_Fee;
 
-//public class GetListingFeeBalanceByClientId
-//{
-//    public class GetListingFeeBalanceByClientIdQuery : IRequest<Result>
-//    {
-//        public int ClientId { get; set; }
-//    }
+[Route("api/listing-fee"), ApiController]
+public class GetListingFeeBalanceByClientId : ControllerBase
+{
+    private readonly IMediator _mediator;
+    public GetListingFeeBalanceByClientId(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
 
-//    public class GetListingFeeBalanceByClientQueryResult
-//    {
-//        public int ClientId { get; set; }
-//        public string FullName { get; set; }
-//        public decimal RemainingBalance { get; set; }
-//    }
+    [HttpGet("{id}")]
+    public async Task<IActionResult> Get(int id)
+    {
+        
+            var query = new GetClientOtherExpensesByIdQuery
+            {
+                ClientId = id
+            };
 
-//    public class Handler : IRequestHandler<GetListingFeeBalanceByClientIdQuery, Result>
-//    {
-//        private readonly ArcanaDbContext _context;
-//        public Handler(ArcanaDbContext context)
-//        {
-//            _context = context;
-//        }
+            var result = await _mediator.Send(query);
 
-//        public async Task<Result> Handle(GetListingFeeBalanceByClientIdQuery request, CancellationToken cancellationToken)
-//        {
-//            var listingFees = await _context.ListingFees
-//                .Include(c => c.Client)
-//                .Where(lf => lf.ClientId == request.ClientId)
-//                .ToListAsync();
+            if (result.IsFailure)
+            {
+                return BadRequest(result);
+            }
 
-//            if (!listingFees.Any()) 
-//            {
-//                return ListingFeeErrors.NotFound();
-//            }
+            return Ok(result);
+        
+        //catch (Exception ex)
+        //{
+        //    return BadRequest(ex.Message);
+        //}
+    }
 
-//            var result = listingFees.Select(x => new GetListingFeeBalanceByClientQueryResult
-//            {
-//                ClientId = request.ClientId,
-//                FullName = Client.
-//            });
+    public class GetListingFeeBalanceByClientIdQuery : IRequest<Result>
+    {
+        public int ClientId { get; set; }
+    }
 
-//        }
-//    }
-//}
+    public class GetListingFeeBalanceByClientQueryResult
+    {
+        public int ClientId { get; set; }
+        public string FullName { get; set; }
+        public decimal RemainingBalance { get; set; }
+    }
+
+    public class Handler : IRequestHandler<GetListingFeeBalanceByClientIdQuery, Result>
+    {
+        private readonly ArcanaDbContext _context;
+        public Handler(ArcanaDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Result> Handle(GetListingFeeBalanceByClientIdQuery request, CancellationToken cancellationToken)
+        {
+            var listingFees = await _context.ListingFees
+                .Include(c => c.Client)
+                .Where(lf => lf.ClientId == request.ClientId && 
+                             lf.Status == Status.Approved)
+                .ToListAsync();
+
+            if (!listingFees.Any())
+            {
+                return ListingFeeErrors.NotFound();
+            }
+
+            var result = listingFees.Select(x => new GetListingFeeBalanceByClientQueryResult
+            {
+                ClientId = request.ClientId,
+                FullName = x.Client.Fullname,
+                RemainingBalance = x.Total
+            });
+
+            return Result.Success("testdev");
+        }
+    }
+}
