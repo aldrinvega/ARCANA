bebe723031bc541025cd7bed9ac985da7165ce3f
it.ralphedwardgesguerra@gmail.com <it.ralphedwardgesguerra@gmail.com>
2024-07-09 15:49:27 +0800

    Adjust Approver Settings in ApproverByRange

---
+++ RDF.Arcana.API/Features/Listing Fee/GetListingFeeBalanceByClientId.cs
@@ -0,0 +1,49 @@
+﻿//using RDF.Arcana.API.Common;
+//using RDF.Arcana.API.Data;
+//using RDF.Arcana.API.Features.Listing_Fee.Errors;
+
+//namespace RDF.Arcana.API.Features.Listing_Fee;
+
+//public class GetListingFeeBalanceByClientId
+//{
+//    public class GetListingFeeBalanceByClientIdQuery : IRequest<Result>
+//    {
+//        public int ClientId { get; set; }
+//    }
+
+//    public class GetListingFeeBalanceByClientQueryResult
+//    {
+//        public int ClientId { get; set; }
+//        public string FullName { get; set; }
+//        public decimal RemainingBalance { get; set; }
+//    }
+
+//    public class Handler : IRequestHandler<GetListingFeeBalanceByClientIdQuery, Result>
+//    {
+//        private readonly ArcanaDbContext _context;
+//        public Handler(ArcanaDbContext context)
+//        {
+//            _context = context;
+//        }
+
+//        public async Task<Result> Handle(GetListingFeeBalanceByClientIdQuery request, CancellationToken cancellationToken)
+//        {
+//            var listingFees = await _context.ListingFees
+//                .Include(c => c.Client)
+//                .Where(lf => lf.ClientId == request.ClientId)
+//                .ToListAsync();
+
+//            if (!listingFees.Any()) 
+//            {
+//                return ListingFeeErrors.NotFound();
+//            }
+
+//            var result = listingFees.Select(x => new GetListingFeeBalanceByClientQueryResult
+//            {
+//                ClientId = request.ClientId,
+//                FullName = Client.
+//            });
+
+//        }
+//    }
+//}
